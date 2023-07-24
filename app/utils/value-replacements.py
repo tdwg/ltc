23c482ae2f2e51628bfb9d8dd99379035cdf05f1
Ben Norton <michaelnorton.ben@gmail.com>
2023-07-24 12:26:08 -0400

    Delete app/utils directory
    Cleaning up app folder

--- app/utils/value-replacements.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df2 = pd.read_csv("../data/ltc-set/ltc-terms-list.csv", encoding="utf8")
-df2['is_required'] = df2['is_required'].replace({'Yes': 'True'})
-df2['is_required'] = df2['is_required'].replace({'No': 'False'})
-df2['is_repeatable'] = df2['is_repeatable'].replace({'Yes': 'True'})
-df2['is_repeatable'] = df2['is_repeatable'].replace({'No': 'False'})
-df2.to_csv("../data/ltc-set/ltc-terms-list.csv", index=False)
